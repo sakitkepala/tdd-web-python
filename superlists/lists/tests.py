d653b6f57851944dee033e3225f948b1a3a9667c
sakitkepala <andikapriyotama@gmail.com>
2020-06-21 18:43:03 +0700

    Model untuk item-item list dan migrasi yang terkait

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 
 from lists.views import home_page
 
-from django.template.loader import render_to_string
+from lists.models import Item
 
 
 class TestHomePage(TestCase):
@@ -19,3 +19,22 @@ class TestHomePage(TestCase):
 
         self.assertIn('Satu item list baru', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+class TestItemModel(TestCase):
+
+    def test_simpan_dan_tarik_item(self):
+        item_pertama = Item()
+        item_pertama.text = 'Item list yang (paling) pertama'
+        item_pertama.save()
+
+        item_kedua = Item()
+        item_kedua.text = 'Item yang kedua'
+        item_kedua.save()
+
+        item_tersimpan = Item.objects.all()
+        self.assertEqual(item_tersimpan.count(), 2)
+
+        item_tersimpan_pertama = item_tersimpan[0]
+        item_tersimpan_kedua = item_tersimpan[1]
+        self.assertEqual(item_tersimpan_pertama.text, 'Item list yang (paling) pertama')
+        self.assertEqual(item_tersimpan_kedua.text, 'Item yang kedua')
