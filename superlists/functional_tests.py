e80a876da1ded869f95346039f328f56f2bca946
sakitkepala <andikapriyotama@gmail.com>
2020-06-21 12:58:15 +0700

    Sekarang functional test cek apakah bisa input item todo

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class TestVisitorBaru(unittest.TestCase):
@@ -17,20 +19,36 @@ class TestVisitorBaru(unittest.TestCase):
         # Dia memperhatikan title dan header halamannya
         # menyebut todo list
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Test selesai!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Dia diajak untuk langsung memasukkan satu item todo
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Masukkan satu item to-do'
+        )
 
         # Dia mengetikkan "Beli bulu merak" ke dalam text box
         # (hobi MJ mengikat umpan pancing pemikat ikan)
+        inputbox.send_keys('Beli bulu merak')
 
         # Ketika pencet enter, halaman terupdate, dan sekarang
         # halamannya menampilkan list "1: Beli bulu merak"
         # sebagai salah satu item yang ada di list todo
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Beli bulu merak' for row in rows)
+        )
 
         # Masih ada text box yang ajak dia isikan item lagi. Dia
         # masukkan "Pakai bulu merak untuk membuat umpan pancing
         # pemikat" (MJ memang agak metodis).
+        self.fail('Testnya diselesaikan!')
 
         # Halamannya terupdate lagi, dan sekarang menampilkan
         # kedua item dalam listnya.
