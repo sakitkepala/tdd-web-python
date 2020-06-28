95c0758f285b518dc9f7e6fd5d5479f116c07a86
sakitkepala <andikapriyotama@gmail.com>
2020-06-28 11:26:41 +0700

    [IMP] FT awal untuk layout & styling

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -122,3 +122,28 @@ class TestVisitorBaru(LiveServerTestCase):
         self.assertIn('Beli susu', teks_halaman)
 
         # Setelah puas, mereka berdua balik tidur lagi masing-masing
+
+    def test_layout_dan_styling(self):
+        # MJ ke halaman home
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Dia lihat box inputnya ada di tengah dengan apik
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # Dia bikin list baru dan lihat inputnya di sana juga
+        # ada di tengah dengan apik
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.tunggu_row_di_tabel_list('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
